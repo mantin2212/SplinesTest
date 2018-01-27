afe9dfbd5cbbbee80364ac77da37f634cc1a9f57
noam mantin <noam mantin@gmail.com>
2018-01-27 11:15:57 +0200

    fixed RobotLocation

--- SplinesTest/src/org/usfirst/frc/team2212/robot/utils/RobotLocation.java
+++ SplinesTest/src/org/usfirst/frc/team2212/robot/utils/RobotLocation.java
@@ -3,39 +3,73 @@ package org.usfirst.frc.team2212.robot.utils;
 import java.util.function.Supplier;
 
 import routes.ArgPoint;
+import utils.Point;
 
 public class RobotLocation {
 
 	private ArgPoint location;
 
-	private double distance;
-	private double dt;
+	private double prevDistance;
+	private double prevAngle;
 
 	private Supplier<Double> getAngle;
 	private Supplier<Double> getDistance;
 
 	public RobotLocation(ArgPoint initLocation, double dt, Supplier<Double> getAngle, Supplier<Double> getDistance) {
-
+		// initializing the robot's location
 		this.location = new ArgPoint(initLocation);
-
-		this.dt = dt;
-		this.distance = 0;
-
+		// initializing class variables
 		this.getAngle = getAngle;
 		this.getDistance = getDistance;
+		// setting the previous distance and angle to their values
+		this.prevDistance = getDistance.get();
+		this.prevAngle = getAngle.get();
 	}
 
 	public void update() {
-		double dl = getDistance.get() - distance;
-		distance += dl;
+		// calculating the current distance and angle
+		double currentDistance = getDistance.get();
+		double currentAngle = getAngle.get();
 
-		location.setAngle(getAngle.get());
+		// setting the angle of the robot's location
+		location.setAngle(currentAngle);
 
-		location.move(dl * Math.cos(location.getAngle()), dl * Math.sin(location.getAngle()));
+		// setting the location's coordinates
+		Point difference = getDifference(prevAngle, currentAngle, currentDistance - prevDistance);
+		location.move(difference.getX(), difference.getY());
+
+		// updating class variables
+		prevDistance = currentDistance;
+		prevAngle = currentAngle;
 	}
 
 	public ArgPoint getLocation() {
 		return location;
 	}
 
+	private Point getDifference(double previousAngle, double currentAngle, double distance) {
+		// calculating the x and y values of the difference
+		double dx = getDx(previousAngle, currentAngle, distance);
+		double dy = getDy(previousAngle, currentAngle, distance);
+
+		return new Point(dx, dy);
+	}
+
+	private double getDx(double previousAngle, double currentAngle, double distance) {
+		double r = distance / (currentAngle - previousAngle);
+
+		double result = -2 * r * Math.sin((currentAngle - previousAngle) / 2)
+				* Math.cos((currentAngle + previousAngle) / 2);
+
+		return result;
+	}
+
+	private double getDy(double previousAngle, double currentAngle, double distance) {
+		double r = distance / (currentAngle - previousAngle);
+
+		double result = -2 * r * Math.sin((currentAngle - previousAngle) / 2)
+				* Math.sin((currentAngle + previousAngle) / 2);
+
+		return result;
+	}
 }
