90413557bc1f365045165f6167d99cf9e29d7ab3
noam mantin <noam mantin@gmail.com>
2018-02-01 23:59:14 +0200

    added relative acceleration to RobotLocationAccelerometers

--- SplinesTest/src/org/usfirst/frc/team2212/robot/utils/location/RobotLocationAccelerometers.java
+++ SplinesTest/src/org/usfirst/frc/team2212/robot/utils/location/RobotLocationAccelerometers.java
@@ -2,26 +2,31 @@ package org.usfirst.frc.team2212.robot.utils.location;
 
 import java.util.function.Supplier;
 
+import org.usfirst.frc.team2212.robot.utils.Utils;
+
 import routes.ArgPoint;
 import utils.Point;
 
 public class RobotLocationAccelerometers extends RobotLocation {
 
-	private Supplier<Double> getXAcceleration;
-	private Supplier<Double> getYAcceleration;
+	private Supplier<Double> getSideAcceleration;
+	private Supplier<Double> getForwardAcceleration;
 
 	private Point velocity;
 	private Point lastVelocity;
 
+	private Point lastAcceleration;
 	private Point acceleration;
 
+	private Point relativeAcceleration;
+
 	public RobotLocationAccelerometers(ArgPoint initLocation, Supplier<Double> getRelativeTime,
-			Supplier<Double> getXAcceleration, Supplier<Double> getYAcceleration) {
+			Supplier<Double> getSideAcceleration, Supplier<Double> getForwardAcceleration) {
 
 		super(initLocation, getRelativeTime);
 
-		this.getXAcceleration = getXAcceleration;
-		this.getYAcceleration = getYAcceleration;
+		this.getSideAcceleration = getSideAcceleration;
+		this.getForwardAcceleration = getForwardAcceleration;
 
 		this.acceleration = new Point(0, 0);
 
@@ -31,20 +36,32 @@ public class RobotLocationAccelerometers extends RobotLocation {
 	public void updateData() {
 		super.updateData();
 
-		acceleration.setXAndY(getXAcceleration.get(), getYAcceleration.get());
+		// receiving the relative acceleration from the suppliers
+		relativeAcceleration.setXAndY(getSideAcceleration.get(), getForwardAcceleration.get());
 
-		updateVelocity();
+		// updating the last acceleration and velocity
+		updatePrevs();
+
+		// calculating the velocity vector and the robot's angle
+		this.velocity.move(timeDiff * lastAcceleration.getX(), timeDiff * lastAcceleration.getY());
+
+		location.setAngle(Math.atan2(velocity.getY(), velocity.getX()));
+
+		// calculating the absolute acceleration vector
+		acceleration = Utils.getAbsoluteVector(relativeAcceleration, location.getAngle());
 	}
 
-	private void updateVelocity() {
+	private void updatePrevs() {
+
 		this.lastVelocity.setXAndY(velocity.getX(), velocity.getY());
 
-		this.velocity.move(timeDiff * acceleration.getX(), timeDiff * acceleration.getY());
+		this.lastAcceleration.setXAndY(acceleration.getX(), acceleration.getY());
 	}
 
 	protected void updateLocation() {
-		double dx = timeDiff * velocity.getX() - 0.5 * Math.pow(timeDiff, 2) * acceleration.getX();
-		double dy = timeDiff * velocity.getY() - 0.5 * Math.pow(timeDiff, 2) * acceleration.getY();
+		// calculating the dx and dy values
+		double dx = timeDiff * velocity.getX() - 0.5 * Math.pow(timeDiff, 2) * lastAcceleration.getX();
+		double dy = timeDiff * velocity.getY() - 0.5 * Math.pow(timeDiff, 2) * lastAcceleration.getY();
 
 		this.velocity.move(dx, dy);
 	}
